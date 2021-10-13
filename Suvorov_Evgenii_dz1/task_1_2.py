5b7b6908ffd65f1efeca9f30ce257a3aed476eb1
suep1984 <89274612+suep1984@users.noreply.github.com>
2021-10-13 21:15:29 +0500

    add README.md

--- Suvorov_Evgenii_dz1/task_1_2.py
+++ Suvorov_Evgenii_dz1/task_1_2.py
@@ -3,8 +3,6 @@ for number in range(0, 1000):
     if number % 2:
         numbers.append(number ** 3)
 
-print('Список кубов нечетных чисел от 1 до 1000: \n\n', numbers, '\n')
-
 sum_of_numbers = 0
 
 for number in numbers:
@@ -17,23 +15,19 @@ for number in numbers:
     if not sum_of_digits % 7:
         sum_of_numbers += number
 
-print('Сумма элементов списка, сумма цифр которых кратна 7 равна ', sum_of_numbers, '\n')
-
-for index_of_number in range(len(numbers)):
-    numbers[index_of_number] += 17
-
-print('Измененный список: \n\n', numbers, '\n')
+print(sum_of_numbers)
 
 new_sum_of_numbers = 0
 
 for number in numbers:
     sum_of_digits = 0
-    num = number
+    new_number = number + 17
+    num = new_number
     while num:
         digit = num % 10
         sum_of_digits += digit
         num //= 10
     if not sum_of_digits % 7:
-        new_sum_of_numbers += number
+        new_sum_of_numbers += new_number
 
-print('Сумма элементов измененного списка, сумма цифр которых кратна 7 равна ', new_sum_of_numbers, '\n')
+print(new_sum_of_numbers)
