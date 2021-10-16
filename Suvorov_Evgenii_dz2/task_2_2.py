02a096ec8827be266d281a9f667fba69c9af3bd5
suep1984 <89274612+suep1984@users.noreply.github.com>
2021-10-16 16:08:39 +0500

    dz_2

--- Suvorov_Evgenii_dz2/task_2_2.py
+++ Suvorov_Evgenii_dz2/task_2_2.py
@@ -1,31 +1,21 @@
 my_lst = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-
+my_lst2 = []
 for item in my_lst:
-    try:
-        if type(int(item)) == int:
-            item_int = int(item)
-            item_str = str(item)
-            if item_str[0] == chr(43) and 0 <= item_int < 10:
-                item_str = f'"{item_str[0]}0{item_int}"'
-                print(item_str, end=' ')
-            elif item_str[0] == chr(43) and item_int >= 10:
-                item_str = f'"{item_str[0]}{item_int}"'
-                print(item_str, end=' ')
-                # my_lst2.append(item_str)
-                # my_lst2.insert(my_lst2.index(item_str), '"')
-                # my_lst2.insert((my_lst2.index(item_str) + 1), '"')
-            elif 0 <= item_int < 10:
-                item_int = f'"0{item_int}"'
-                print(item_int, end=' ')
-                # my_lst2.append(item_int)
-                # my_lst2.insert(my_lst2.index(item_int), '"')
-                # my_lst2.insert((my_lst2.index(item_int) + 1), '"')
-            else:
-                item_int = f'"{item_int}"'
-                print(item_int, end=' ')
-                # my_lst2.append(item_int)
-                # my_lst2.insert(my_lst2.index(item_int), '"')
-                # my_lst2.insert((my_lst2.index(item_int) + 1), '"')
-    except ValueError:
-        print(item, end=' ')
-        # my_lst2.append(item)
+    if '0' <= item[0] <= '9':
+        item = str(f'{int(item):02d}')
+        my_lst2.append(item)
+        my_lst2.insert(my_lst2.index(item), '"')
+        my_lst2.insert((my_lst2.index(item) + 1), '"')
+    elif item[0] == '+':
+        item = str(f'{item[0]}{int(item[1:]):02d}')
+        my_lst2.append(item)
+        my_lst2.insert(my_lst2.index(item), '"')
+        my_lst2.insert((my_lst2.index(item) + 1), '"')
+    else:
+        my_lst2.append(item)
+
+print(my_lst2)
+
+print(f'{my_lst2[0]} {"".join(my_lst2[1:4])} {my_lst2[4]} '
+      f'{"".join(my_lst2[5:8])} {" ".join(my_lst2[8:12])} '
+      f'{"".join(my_lst2[12:15])} {my_lst2[15]}')
--- Suvorov_Evgenii_dz2/task_2_3.py
+++ Suvorov_Evgenii_dz2/task_2_3.py
@@ -1,7 +1,7 @@
 my_lst = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
 for item in my_lst:
-    if 48 <= ord(item[0]) <= 57:
+    if '0' <= item[0] <= '9':
         print(f'"{int(item):02d}"', end=' ')
     elif item[0] == '+':
         print(f'"{item[0]}{int(item):02d}"', end=' ')
