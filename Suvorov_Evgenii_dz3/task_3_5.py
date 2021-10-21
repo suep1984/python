fa38e3ab9ab390d8745101a7b870278ddcabfaca
suep1984 <89274612+suep1984@users.noreply.github.com>
2021-10-21 23:19:21 +0500

    dz_3

--- Suvorov_Evgenii_dz3/task_3_5.py
+++ Suvorov_Evgenii_dz3/task_3_5.py
@@ -5,7 +5,19 @@ adverbs = ["сегодня", "вчера", "завтра", "позавчера",
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 
-def get_jokes(n):
+def get_jokes(count_jokes=3, repeat=True):
     """Jokes generator"""
-    jokes = [f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}' for i in range(n)]
+    jokes = []
+    if not repeat:
+        for i in range(count_jokes):
+            word1 = choice(nouns)
+            word2 = choice(adverbs)
+            word3 = choice(adjectives)
+            jokes.append(f'{word1} {word2} {word3}')
+            nouns.pop(nouns.index(word1))
+            adverbs.pop(adverbs.index(word2))
+            adjectives.pop(adjectives.index(word3))
+    else:
+        jokes = [f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}' for i in range(count_jokes)]
+
     return jokes
