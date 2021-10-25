9e7fda6c8d494ae33720395ebc3201ab040746c7
suep1984 <89274612+suep1984@users.noreply.github.com>
2021-10-25 21:55:04 +0500

    dz_5

--- Suvorov_Evgenii_dz4/task_4_4.py
+++
@@ -1,6 +0,0 @@
-import utils
-
-
-if __name__ == '__main__':
-    example_cur, example_date = utils.currency_rates(input())
-    print(f' {example_cur}, {example_date}')
--- Suvorov_Evgenii_dz4/task_4_5.py
+++
@@ -1,9 +0,0 @@
-import utils
-import sys
-
-if __name__ == '__main__':
-    example_cur, example_date = utils.currency_rates(sys.argv[1])
-    if len(sys.argv) == 2:
-        print(f' {example_cur}, {example_date}')
-    else:
-        print(None)
